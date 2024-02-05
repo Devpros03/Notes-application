ab9fc4005478b78816ba223624251d0be39f4cee
Amit <amit@gmail.com>
2024-02-05 11:01:29 -0500

    test commit amit

--- app/src/main/java/com/example/notesapplication/MainActivity.java
+++ app/src/main/java/com/example/notesapplication/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //main file code 123
+        //amit here
     }
 }
 
